b5d9b0208769434170b26e9c1e238127d746e549
Afshanda <afshanda.jabeen@hotmail.com>
2021-07-14 16:57:45 +0500

    Initial Commit

---
+++ onestopshop/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for onestopshop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onestopshop.settings')
+
+application = get_wsgi_application()
