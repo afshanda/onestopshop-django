b7044d839659a05c5b1293656b3a1b976533c4de
Afshanda <afshanda.jabeen@hotmail.com>
2021-08-02 14:19:17 +0500

    allowed host

--- onestopshop/settings.py
+++ onestopshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast = bool)
 
-ALLOWED_HOSTS = ['onestopstores-env.eba-dkmw2f5c.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['onestopshop-django-env.eba-cwxvkebv.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
